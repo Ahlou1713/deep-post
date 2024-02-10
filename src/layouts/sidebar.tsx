2e45b283325da25aabbaa7f410680cafa3944a2f
Melyna <melynaferiel.soudani@gmail.com>
2024-02-10 12:11:59 +0100

    lastpush

--- src/layouts/sidebar.tsx
+++ src/layouts/sidebar.tsx
@@ -5,20 +5,21 @@ import MenuItem from "antd/es/menu/MenuItem";
 import { CodeOutlined, HomeFilled, UserOutlined } from "@ant-design/icons";
 import { Content } from "antd/es/layout/layout";
 import UserAvatar from "../components/UserAvatar";
+import MyCard from "../components/Card";
 
 const { Sider } = Layout;
 
 const siderStyle: React.CSSProperties = {
-  textAlign: 'center',
-  lineHeight: '120px',
-  backgroundColor: '#16171A',
-  color: '#fff'
+  textAlign: "center",
+  lineHeight: "120px",
+  backgroundColor: "#16171A",
+  color: "#fff",
 };
 
 const menuStyle: React.CSSProperties = {
-  textAlign: 'center',
-  lineHeight: '120px',
-  backgroundColor: '#16171A',
+  textAlign: "center",
+  lineHeight: "120px",
+  backgroundColor: "#16171A",
 };
 
 type MenuItem = Required<MenuProps>["items"][number];
@@ -59,10 +60,34 @@ const SideBar = ({ children }: Props) => {
     token: { colorBgContainer },
   } = theme.useToken();
 
+  const CardData = [
+    {
+      imageUrl: "src/assets/Hackshark V2.png",
+      title: "Hackshark V2",
+      Date: "14/12/2024",
+      participants: 150,
+      prize: "10000DA",
+    },
+    {
+      imageUrl: "src/assets/GGC.png",
+      title: "Gamer Jam",
+      Date: "20/05/2024",
+      participants: 150,
+      prize: "10000DA",
+    },
+    {
+      imageUrl: " src/assets/Partyrock.png",
+      title: " ",
+      Date: "20/05/2024",
+      participants: 50,
+      prize: "10000DA",
+    },
+  ];
+
   return (
     <Layout style={{ minHeight: "100vh", background: colorBgContainer }}>
       <Sider
-      style={siderStyle}
+        style={siderStyle}
         collapsible
         collapsed={collapsed}
         onCollapse={(value) => setCollapsed(value)}
@@ -76,7 +101,18 @@ const SideBar = ({ children }: Props) => {
         />
         <UserAvatar>Alla Eddine Rakik</UserAvatar>
       </Sider>
-      <Content style={{ background: '#16171A' }}></Content>
+      <Content style={{ background: "#16171A" }}>
+        {CardData.map((card, index) => (
+          <MyCard
+            key={index}
+            imageUrl={card.imageUrl}
+            title={card.title}
+            date={card.Date}
+            participants={card.participants}
+            prize={card.prize}
+          />
+        ))}
+      </Content>
     </Layout>
   );
 };
